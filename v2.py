e5d2dac32391bd868355a8aa23e4b2af49696f9a
NakshOpDev <naksh9672@gmail.com>
2025-05-01 13:30:31 +0530

    Update v2.py

--- v2.py
+++ v2.py
@@ -12,7 +12,7 @@ import docker
 import asyncio
 from discord import app_commands
 
-TOKEN = '' # TOKEN HERE
+TOKEN = 'MTM2NzQwMzI1MzMzOTUyMTA5NA.GU_kJI.3TRLufMNtv__LSiAw_6h3PVOgZBbIghClud1yc' # TOKEN HERE
 RAM_LIMIT = '2g'
 SERVER_LIMIT = 12
 database_file = 'database.txt'
